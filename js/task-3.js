a9e35c6713b6444bc9cbdb49c89c55b4ce17f2ff
yangelia <yana.sychova@gmail.com>
2025-05-25 17:42:38 +0300

    edits

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,5 @@
 function getElementWidth(content, padding, border) {
-  return `${
-    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border)
-  }`;
+  return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 }
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
